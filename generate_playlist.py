31204aa680b71ce14077b7d7c731b961b4de5d84
Incognito Brothers <195974522+unosottor@users.noreply.github.com>
2025-11-15 20:38:47 +0530

    Update generate_playlist.py

--- generate_playlist.py
+++ generate_playlist.py
@@ -8,7 +8,7 @@ OUTFILE = "playlist.m3u"
 # --- Fixed Promo Channel ---
 PROMO_CHANNEL = {
     "name": "HOME CHANNEL",
-    "tvg-logo": "",
+    "tvg-logo": "https://raw.githubusercontent.com/unosottor/tvg-logo/refs/heads/main/images/joinustelegram.png",
     "group-title": "JOIN TELEGRAM",
     "url": "https://raw.githubusercontent.com/unosottor/unosottor.github.io/refs/heads/main/notice/index.m3u8"
 }
